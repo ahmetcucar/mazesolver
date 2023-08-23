b27493f325723c3eff6f3da3c550995e298c36cb
ahmetcucar <acucar@uw.edu>
2023-08-22 18:55:20 -0700

    cell class

--- main.py
+++ main.py
@@ -43,11 +43,90 @@ class Line:
         )
         canvas.pack()
 
+class Cell:
+    def __init__(self, left, right, top, bottom, x1, y1, x2, y2, window):
+        self.has_left_wall = left
+        self.has_right_wall = right
+        self.has_top_wall = top
+        self.has_bottom_wall = bottom
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        self.__window = window
+
+    def get_center(self):
+        return Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+
+    def draw(self):
+        if self.has_left_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)), "black"
+            )
+        if self.has_right_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)), "black"
+            )
+        if self.has_top_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)), "black"
+            )
+        if self.has_bottom_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), "black"
+            )
+
+    def draw_move(self, to_cell, undo=False):
+        # determine the positioning of to_cell relative to self and see if
+        # there is a wall blocking us
+        x_diff = to_cell.get_center().x - self.get_center().x
+        y_diff = to_cell.get_center().y - self.get_center().y
+        if x_diff != 0 and y_diff != 0:
+            raise Exception("Can only move to adjacent cells")
+        if x_diff == 0:
+            if y_diff < 0 and self.has_top_wall:
+                raise Exception("Wall in the way")
+            if y_diff > 0 and self.has_bottom_wall:
+                raise Exception("Wall in the way")
+        else:
+            if x_diff < 0 and self.has_left_wall:
+                raise Exception("Wall in the way")
+            if x_diff > 0 and self.has_right_wall:
+                raise Exception("Wall in the way")
+
+        # draw the line
+        color = "gray" if undo else "red"
+        self.__window.draw_line(
+            Line(self.get_center(), to_cell.get_center()), color
+        )
+
 
 def main():
     window = Window(800, 600)
-    window.draw_line(Line(Point(0, 0), Point(800, 600)), "red")
-    window.draw_line(Line(Point(0, 600), Point(800, 0)), "blue")
+
+    # create the cells to fill the window, each having any number of walls
+    cells = []
+    cell_width = 200
+    cell_height = 200
+    for x in range(0, window._Window__width, cell_width):
+        for y in range(0, window._Window__height, cell_height):
+            cells.append(
+                Cell(
+                    True,
+                    True,
+                    True,
+                    True,
+                    x,
+                    y,
+                    x + cell_width,
+                    y + cell_height,
+                    window,
+                )
+            )
+    for cell in cells:
+        cell.draw()
+
+
     window.wait_for_close()
 
 main()
