905376415081f2537742be8832353c20d2035871
ahmetcucar <acucar@uw.edu>
2023-08-22 17:56:19 -0700

    point and line class

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, color):
+        line.draw(self.__canvas, color)
+
     def redraw(self):
         self.__root.update()
         self.__root.update_idletasks()
@@ -24,8 +27,27 @@ class Window:
     def close(self):
         self.__running = False
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, color):
+        canvas.create_line(
+            self.start.x, self.start.y, self.end.x, self.end.y, fill=color, width=2
+        )
+        canvas.pack()
+
+
 def main():
     window = Window(800, 600)
+    window.draw_line(Line(Point(0, 0), Point(800, 600)), "red")
+    window.draw_line(Line(Point(0, 600), Point(800, 0)), "blue")
     window.wait_for_close()
 
 main()
